72b10f064e1e5bfbcfb4359514da6bd61eeca1e4
ahmedtom <101574426+ahmedtom@users.noreply.github.com>
2022-12-21 02:51:45 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import pyrogram from Client, filters
 
 bot = Client(
     'Bot', 
-    api_id = 16756774 , 
+    api_id = 16756774, 
     api_hash ='9bc4030d639c431fae3383c0427bf0ee', 
     bot_token ='5829316810:AAHu15gMm8Js9f6Hj1o-YfAwrbjgh2p5pA0'
 )
